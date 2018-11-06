382eab366658901dc9a33f1d9e87b04218a48aee
DanChiquito <43891363+DanChiquito@users.noreply.github.com>
2018-11-06 07:42:16 -0600

    Add files via upload

---
+++ Videojugo/VideoJuego/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace VideoJuego
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbPlat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.Location = new System.Drawing.Point(195, 37);
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtbNombre.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Título del Juego:";
+            // 
+            // txtbPlat
+            // 
+            this.txtbPlat.Location = new System.Drawing.Point(195, 80);
+            this.txtbPlat.Name = "txtbPlat";
+            this.txtbPlat.Size = new System.Drawing.Size(100, 22);
+            this.txtbPlat.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plataforma:";
+            // 
+            // txtbPrecio
+            // 
+            this.txtbPrecio.Location = new System.Drawing.Point(195, 126);
+            this.txtbPrecio.Name = "txtbPrecio";
+            this.txtbPrecio.Size = new System.Drawing.Size(100, 22);
+            this.txtbPrecio.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Numero de Copias";
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Location = new System.Drawing.Point(417, 55);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(132, 93);
+            this.btnComprar.TabIndex = 2;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Resultado Compra";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Resultado Compra";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(258, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Resultado Compra";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(258, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 17);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Resultado Compra";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(258, 360);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 17);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Resultado Compra";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(258, 332);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(125, 17);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Resultado Compra";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(258, 389);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(125, 17);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Resultado Compra";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(269, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(261, 17);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "CUALQUIER JUEGO EN 1099 PESITOS";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(38, 218);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(105, 17);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "Título del juego";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(38, 246);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(76, 17);
+            this.label14.TabIndex = 1;
+            this.label14.Text = "Plataforma";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(38, 275);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(122, 17);
+            this.label15.TabIndex = 1;
+            this.label15.Text = "Numero de discos";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(38, 305);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(112, 17);
+            this.label16.TabIndex = 1;
+            this.label16.Text = "Servicio TIENDA";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(38, 360);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(162, 17);
+            this.label17.TabIndex = 1;
+            this.label17.Text = "Estado de la adquisicion";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(38, 332);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(96, 17);
+            this.label18.TabIndex = 1;
+            this.label18.Text = "Dsiponibilidad";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(38, 389);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(95, 17);
+            this.label19.TabIndex = 1;
+            this.label19.Text = "Total A Pagar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbPrecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbPlat);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbPlat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbPrecio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+    }
+}
+
---
+++ Videojugo/VideoJuego/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoJuego
+{
+    public partial class Form1 : Form
+    {
+        string[] misdatos = new string[3];
+
+        public struct Adquisicion
+        {
+            public string compra;
+            public string disponibilidad;
+            public int total;
+        }
+        public struct Tienda
+        {
+            public string servicio;
+            public Juego juego;
+            public Adquisicion adquisicion;
+        }
+        public struct Juego
+        {
+            public string plataforma;
+            public byte numDiscos;
+            public string titulo;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Tienda[] misTiendas = new Tienda[1];
+        }
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            Tienda[] misTiendas = new Tienda[1];
+
+            try
+            {
+
+                misdatos[0] = txtbNombre.Text;
+                misdatos[1] = txtbPlat.Text;
+                misdatos[2] = txtbPrecio.Text;
+                for (int i = 0; i < misdatos.Length; i++)
+                {
+                    string aux = misdatos[i];
+                    if (aux == "")
+                    {
+                        throw new ApplicationException();
+
+                    }
+
+                }
+
+                MessageBox.Show("Se esta checando la venta presione para continuar\n");
+
+            }
+            catch (ApplicationException)
+            {
+                MessageBox.Show("revise bien qu elos campos esten completos porfavor");
+
+            }
+
+            misTiendas[0].servicio = "Servicio : Encontrado y empaquetado";
+            misTiendas[0].juego.titulo = misdatos[0];
+            misTiendas[0].juego.plataforma = misdatos[1];
+            misTiendas[0].juego.numDiscos = byte.Parse(misdatos[2]);
+            misTiendas[0].adquisicion.disponibilidad = "Si esta disponible";
+            misTiendas[0].adquisicion.compra = "Su compra fue exitosa con pago en efectivo";
+            misTiendas[0].adquisicion.total = byte.Parse(misdatos[2]) * 1099;
+
+
+            label4.Text = misdatos[0];
+            label5.Text = misdatos[1];
+            label6.Text = misdatos[2];
+            label7.Text = misTiendas[0].servicio;
+            label8.Text = misTiendas[0].adquisicion.compra;
+            label9.Text = misTiendas[0].adquisicion.disponibilidad;
+            label10.Text = "A pagar:       pesitos" + misTiendas[0].adquisicion.total;
+
+        }
+    }
+}
